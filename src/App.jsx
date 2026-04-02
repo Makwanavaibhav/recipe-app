95de1cbfab2279943a8f9afb99cd12f7be5ea420
Makwanavaibhav <vaibhavmakwana617@gmail.com>
2026-04-02 13:36:47 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import AppNavbar from "./components/layout/AppNavbar";
 import Dashboard from "./pages/Dashboard";
 import Search from "./pages/Search";
